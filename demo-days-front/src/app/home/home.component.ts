22d6cc067996f42517e00b4f7f2b40c9ab8286db
Rustem_Murzagaliyev <r_murzagaliyev@kbtu.kz>
2026-04-09 11:14:36 +0500

    angular

---
+++ demo-days-front/src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, NgbModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {}
