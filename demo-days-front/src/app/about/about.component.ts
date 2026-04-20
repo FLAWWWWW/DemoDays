19f98716b3fc1ef27b5a87d54f8e44310233a390
Rustem_Murzagaliyev <r_murzagaliyev@kbtu.kz>
2026-04-20 16:37:49 +0500

    about

---
+++ demo-days-front/src/app/about/about.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-account',
+  standalone: true,
+  // ОБЯЗАТЕЛЬНО добавь эти два модуля в imports ниже
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+  constructor(private router: Router) {}
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    surname: new FormControl(''),
+    phone: new FormControl(''),
+    email: new FormControl({value: '', disabled: true}), // Почту обычно менять нельзя без подтверждения
+    bio: new FormControl(''),
+    role: new FormControl('Guest')
+  });
+
+  onLogout() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    this.router.navigate(['/']); // редирект на главную или страницу логина
+  }
+
+  passwordForm = new FormGroup({
+    oldPassword: new FormControl('', [Validators.required]),
+    newPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+}
--- demo-days-front/src/app/app.routes.ts
+++ demo-days-front/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EventComponent } from './event/event.component';
 import { AccountComponent } from './account/account.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'event/:id', component: EventComponent },
   { path: 'account', component: AccountComponent },
+  { path: 'about', component: AboutComponent },
 ];
