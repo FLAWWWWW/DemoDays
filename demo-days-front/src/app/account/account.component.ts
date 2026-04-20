d729d5cab30a0bb5b5e84bbad01f8f4d82801997
Rustem_Murzagaliyev <r_murzagaliyev@kbtu.kz>
2026-04-20 17:58:28 +0500

    Account creating events

--- demo-days-front/src/app/account/account.component.ts
+++ demo-days-front/src/app/account/account.component.ts
@@ -1,28 +1,55 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule, FormArray } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-
-
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  // ОБЯЗАТЕЛЬНО добавь эти два модуля в imports ниже
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
+
 export class AccountComponent {
   constructor(private router: Router) {}
   profileForm = new FormGroup({
     name: new FormControl(''),
     surname: new FormControl(''),
     phone: new FormControl(''),
-    email: new FormControl({value: '', disabled: true}), // Почту обычно менять нельзя без подтверждения
+    email: new FormControl({value: '', disabled: true}),
     bio: new FormControl(''),
     role: new FormControl('Guest')
   });
+
+  presentationForm = new FormGroup({
+    presentationName: new FormControl('', [Validators.required]),
+    participantCount: new FormControl(1),
+    participants: new FormArray([
+      this.createParticipantGroup()
+    ])
+  });
+
+  createParticipantGroup(): FormGroup {
+    return new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+    })
+  }
+
+  get participants() {
+    return this.presentationForm.get('participants') as FormArray;
+  }
+
+  addParticipant(){
+    this.participants.push(this.createParticipantGroup());
+  }
+
+  removeParticipant(index: number){
+    if (this.participants.length > 1){
+      this.participants.removeAt(index);
+    }
+  }
 
   onLogout() {
     localStorage.removeItem('access_token');
